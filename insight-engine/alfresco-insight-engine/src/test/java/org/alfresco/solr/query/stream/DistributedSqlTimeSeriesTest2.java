05196de53d80faa053bde409741d4854f8f62371
Tuna Aksoy <tuna.aksoy@alfresco.com>
2017-10-20 16:48:03 +0100

    SEARCH-630: Core waiting fixed

--- insight-engine/alfresco-insight-engine/src/test/java/org/alfresco/solr/query/stream/DistributedSqlTimeSeriesTest2.java
+++ insight-engine/alfresco-insight-engine/src/test/java/org/alfresco/solr/query/stream/DistributedSqlTimeSeriesTest2.java
@@ -26,6 +26,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.solr.client.Node;
@@ -37,6 +38,7 @@ import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.util.LuceneTestCase;
 import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.client.solrj.io.Tuple;
+import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -51,7 +53,8 @@ public class DistributedSqlTimeSeriesTest2 extends AbstractStreamTest
     @Rule
     public JettyServerRule jetty = new JettyServerRule(1, this);
 
-    private int numDocs = 12;
+    //private int numDocs = 12;
+    private int numDocs = 720;
     private Transaction txn = getTransaction(0, numDocs);
     private List<Node> nodes = new ArrayList<>();
     private List<NodeMetaData> nodeMetaDatas = new ArrayList<>();
@@ -68,16 +71,29 @@ public class DistributedSqlTimeSeriesTest2 extends AbstractStreamTest
     @Before
     private void createData() throws Exception
     {
+        TimeZone.setDefault(TimeZone.getTimeZone(DateTimeZone.UTC.getID()));
+
         int currentYear = LocalDateTime.now().getYear();
-        for (int i = 1; i <= numDocs; i++)
+        /*
+        for (int i = 0; i < numDocs; i++)
         {
+            //updateProperties(LocalDateTime.of(currentYear, 10, 20, i, 0, 0));
             updateProperties(LocalDateTime.of(currentYear, 1, i, 0, 0, 0));
             updateProperties(LocalDateTime.of(currentYear + 1, i, 1, 0, 0, 0));
             updateProperties(LocalDateTime.of(currentYear - i, 1, 1, 0, 0, 0));
         }
+        */
+
+        for (int i = 1; i <= 30; i++)
+        {
+            for (int j = 0; j < 24; j++)
+            {
+                updateProperties(LocalDateTime.of(currentYear, 10, i, j, 0, 0));
+            }
+        }
 
         indexTransaction(txn, nodes, nodeMetaDatas);
-        waitForDocCount(new TermQuery(new Term("content@s___t@{http://www.alfresco.org/model/content/1.0}content", "world")), numDocs * 3 + 4, 80000);
+        waitForDocCount(new TermQuery(new Term("content@s___t@{http://www.alfresco.org/model/content/1.0}content", "world")), numDocs + 4, 80000);
     }
 
     private void updateProperties(LocalDateTime localDateTime)
