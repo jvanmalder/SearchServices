1f693c828782118682d0d7f6c110a3d310fe3540
eliaporciani <elia.porciani@gmail.com>
2018-12-17 18:13:18 +0100

    [SEARCH-873] handled predicates with operator "not in (...)"

--- insight-engine/alfresco-insight-engine/src/main/java/org/alfresco/solr/sql/SolrSchemaUtil.java
+++ insight-engine/alfresco-insight-engine/src/main/java/org/alfresco/solr/sql/SolrSchemaUtil.java
@@ -49,7 +49,7 @@ public class SolrSchemaUtil
                 String[] conjunctionAndDisjunction = sqlpred[1].split("(?i)and | or");
                 for(int i = 0; i < conjunctionAndDisjunction.length; i++)
                 {
-                    String predic = conjunctionAndDisjunction[i].split("[><!~]=?|<>|=| in ")[0].trim();
+                    String predic = conjunctionAndDisjunction[i].split("[><!~]=?|<>|=| in | not ")[0].trim();
                     if(!predic.startsWith("'"))
                     {
                         predicates.add(predic.replaceAll("`", ""));
